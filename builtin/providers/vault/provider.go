8c4fbd59eb8e3c2090e3caf57dfdcb9de53bad38
Paul Hinze <phinze@phinze.com>
2016-01-28 17:56:19 -0600

    provider/vault: Groundwork and initial resource
    First resource: "vault_secret_backend"

---
+++ builtin/providers/vault/provider.go
@@ -0,0 +1,37 @@
+package vault
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/vault/api"
+)
+
+// Provider returns a schema.Provider for managing Packet infrastructure.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"address": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"vault_secret_backend": resourceVaultSecretBackend(),
+		},
+
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := api.DefaultConfig()
+	if v, ok := d.GetOk("address"); ok {
+		config.Address = v.(string)
+	}
+	client, err := api.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
