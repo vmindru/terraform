33bc21fd3cd5bcc3f0b476eaa54f449921609f74
Paul Hinze <phinze@phinze.com>
2016-02-02 10:56:16 -0600

    provider/vault: Add resource "vault_auth_backend"

--- builtin/providers/vault/provider.go
+++ builtin/providers/vault/provider.go
@@ -17,6 +17,7 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
+			"vault_auth_backend":   resourceVaultAuthBackend(),
 			"vault_secret_backend": resourceVaultSecretBackend(),
 		},
 
